df482349a83d1c1b84e3017355f0afa4a76e4cd9
tejasammu10 <tejasammu22@gmail.com>
2026-09-12 20:34:35 +0530

    37th commit

---
+++ functioncalculator.py
@@ -0,0 +1,20 @@
+
+def add(a, b):
+    pass 
+
+
+def subtract(a, b):
+    pass
+
+def multiply(a, b):
+    pass
+
+
+def divide(a, b):
+    pass
+
+print("=" * 36)
+print("     🧮  FUNCTION CALCULATOR")
+print("=" * 36)
+print("Operations: add | subtract | multiply | divide")
+print()
